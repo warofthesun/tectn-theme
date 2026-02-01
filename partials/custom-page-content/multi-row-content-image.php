8d94614c3d2e13061277b247b5d7209d7b4df3d4
Dan Brown <dan@thesplitpixel.com>
2026-02-01 18:43:56 -0500

    Add row two content and styling

--- partials/custom-page-content/multi-row-content-image.php
+++ partials/custom-page-content/multi-row-content-image.php
@@ -25,18 +25,7 @@
         }
       }
 
-endif;
-    if( get_row_layout() == 'multi_row_content_image'): 
-    $row_two = get_sub_field('row_two');
-    $headline_r2       = $row_two['headline'] ?? '';
-    $headline_size_r2  = $row_two['headline_size'] ?? '';
-    $body_r2           = $row_two['body_copy'] ?? '';
-    $primary_cta_r2    = $row_two['primary_cta'] ?? null;   
-    $secondary_cta_r2  = $row_two['secondary_cta'] ?? null; 
-    $supporting_images_r2  = $row_two['supporting_images'] ?? null;
-endif; ?>
-
-<?php 
+endif; 
     $classes = ['content_group'];
     if ( $row_one )  $classes[] = 'content_group--row_one';
     if ( $content_position === 'middle')  $classes[] = 'content_group--middle';
@@ -78,16 +67,80 @@ endif; ?>
         <?php endif; ?>
     </div>
 </div>
-<?php 
+
+ <?php
+ if( get_row_layout() == 'multi_row_content_image'): 
+ $row_two = get_sub_field('row_two');
+ $headline       = $row_two['headline'] ?? '';
+ $headline_size  = $row_two['headline_size'] ?? '';
+ $body           = $row_two['body_copy'] ?? '';
+ $content_position = $row_two['content_vertical'] ?? null;
+ $image_position = $row_two['image_horizontal'] ?? null;
+ $supporting_images = $row_two['supporting_images'] ?? [];
+ if (!is_array($supporting_images)) $supporting_images = [];
+
+ $buttons_rows     = $row_two['buttons'] ?? [];
+ if (!is_array($buttons_rows)) $buttons_rows = [];
+
+ // Build a clean buttons array you can loop over in markup
+ $buttons = [];
+
+ foreach ($buttons_rows as $button_row) {
+     $link = $button_row['button'] ?? null;
+ 
+     if (is_array($link) && !empty($link['url'])) {
+       $buttons[] = [
+         'url'    => $link['url'],
+         'title'  => $link['title'] ?? 'Learn more',
+         'target' => $link['target'] ?? '_self',
+       ];
+     }
+   }
+endif; 
     $classes = ['content_group'];
     if ( $row_two )  $classes[] = 'content_group--row_two';
+    if ( $content_position === 'middle')  $classes[] = 'content_group--middle';
+    if ( $content_position === 'bottom')  $classes[] = 'content_group--bottom';
+    if ( $image_position === 'left')  $classes[] = 'content_group--reverse';
 ?>
+
 <div class="<?php echo esc_attr(implode(' ', $classes)); ?> row">
-    <div class="col-xs-12 col-md-6">
-        <?php if($headline_r2) : ?><<?php echo esc_html($headline_size_r2); ?>><?php echo esc_html($headline_r2); ?></<?php echo esc_html($headline_size_r2); ?>><?php endif; ?>
-        <?php if($body_r2) : ?><?php echo wp_kses_post($body_r2); ?><?php endif; ?>
+    <div class="col-xs-12 col-md-4 content">
+        <?php if($headline) : ?><<?php echo esc_html($headline_size); ?>><?php echo esc_html($headline); ?></<?php echo esc_html($headline_size); ?>><?php endif; ?>
+        <?php if($body) : ?><?php echo wp_kses_post($body); ?><?php endif; ?>
+        <?php if (!empty($buttons)) : ?>
+        <div class="button_pair">
+            <?php foreach ($buttons as $btn) : ?>
+            <a class="button"
+                href="<?php echo esc_url($btn['url']); ?>"
+                target="<?php echo esc_attr($btn['target']); ?>"
+                <?php echo ($btn['target'] === '_blank') ? 'rel="noopener noreferrer"' : ''; ?>>
+                <?php echo esc_html($btn['title']); ?>
+            </a>
+            <?php endforeach; ?>
+        </div>
+        <?php endif; ?>
     </div>
-    <div class="col-xs-12 col-md-5">
-        featured image
-    </div>
+    <?php if (!empty($supporting_images)) : ?>
+    <ul class="col-xs-12 col-md-8 supporting_images">
+            <?php foreach ($supporting_images as $image) : ?>
+                <li>
+            <figure class="gallery__item">
+                <?php
+                echo wp_get_attachment_image(
+                    $image['ID'],
+                    'large',            // change size as needed: thumbnail, medium, large, full, or custom
+                    false,
+                    [
+                    'class'   => 'gallery__img',
+                    'loading' => 'lazy',
+                    'alt'     => $image['alt'] ?: '', // wp_get_attachment_image usually handles alt, but this is fine too
+                    ]
+                );
+                ?>
+            </figure>
+            </li>
+            <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
 </div>
