e4ab9018630d456227976bc53e90b75e0ff85412
Dan Brown <dan@thesplitpixel.com>
2026-02-21 22:09:16 -0500

    Begin updating color variables and tokens

--- blocks/text-image/text_image.php
+++ blocks/text-image/text_image.php
@@ -95,26 +95,8 @@
             <div class="<?= esc_attr($text_col); ?> content-group__content">
                 <?php if($headline) : ?><<?php echo esc_html($headline_size); ?>><?php echo esc_html($headline); ?></<?php echo esc_html($headline_size); ?>><?php endif; ?>
                 <?php if($body) : ?><?php echo wp_kses_post($body); ?><?php endif; ?>
-                <?php if (have_rows('buttons')) : ?>
-                    <div class="button-pair">
-                        <?php while (have_rows('buttons')) : the_row();
-                        $link = get_sub_field('button');
-                        if (!$link) continue;
-
-                        $url    = $link['url'] ?? '';
-                        $title  = $link['title'] ?? '';
-                        $target = $link['target'] ?? '_self';
-                        if (!$url || !$title) continue;
-                        ?>
-                        <a class="button"
-                            href="<?php echo esc_url($url); ?>"
-                            target="<?php echo esc_attr($target); ?>"
-                            <?php echo ($target === '_blank') ? 'rel="noopener noreferrer"' : ''; ?>>
-                            <?php echo esc_html($title); ?>
-                        </a>
-                        <?php endwhile; ?>
-                    </div>
-                <?php endif; ?>
+                    <?php $partial_path = get_theme_file_path('/partials/button_pair.php'); ?>
+                    <?php include $partial_path; ?>
             </div>
             <div class="<?= esc_attr($image_col); ?>">
             <?php if( $images ) : ?>
